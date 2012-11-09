6eadee4481121fb78b5ceac266af48bab3a7d094
Nicholas Pappas <nwpappas@exceptontuesdays.com>
2012-11-08 21:19:19 -0800

    Initial code commit
    Initial code dump of reusable gesture library for Microsoft Kinect for
    Windows SDK v1.5, including simple demo application.

---
+++ Fizbin.Kinect.Gestures.Demo/MainWindow.xaml.cs
@@ -0,0 +1,231 @@
+﻿using System.Windows;
+using System.Windows.Data;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Samples.Kinect.WpfViewers;
+using System.Diagnostics;
+using System.ComponentModel;
+using System;
+
+namespace Fizbin.Kinect.Gestures.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private readonly KinectSensorChooser sensorChooser = new KinectSensorChooser();
+
+        private Skeleton[] skeletons = new Skeleton[0];
+
+        // skeleton gesture recognizer
+        private GestureController gestureController;
+
+        public MainWindow()
+        {
+            DataContext = this;
+
+            InitializeComponent();
+
+            // initialize the Kinect sensor manager
+            KinectSensorManager = new KinectSensorManager();
+            KinectSensorManager.KinectSensorChanged += this.KinectSensorChanged;
+
+            // locate an available sensor
+            sensorChooser.Start();
+
+            // bind chooser's sensor value to the local sensor manager
+            var kinectSensorBinding = new Binding("Kinect") { Source = this.sensorChooser };
+            BindingOperations.SetBinding(this.KinectSensorManager, KinectSensorManager.KinectSensorProperty, kinectSensorBinding);
+        }
+
+        #region Kinect Discovery & Setup
+
+        private void KinectSensorChanged(object sender, KinectSensorManagerEventArgs<KinectSensor> args)
+        {
+            if (null != args.OldValue)
+                UninitializeKinectServices(args.OldValue);
+
+            if (null != args.NewValue)
+                InitializeKinectServices(KinectSensorManager, args.NewValue);
+        }
+
+        /// <summary>
+        /// Kinect enabled apps should customize which Kinect services it initializes here.
+        /// </summary>
+        /// <param name="kinectSensorManager"></param>
+        /// <param name="sensor"></param>
+        private void InitializeKinectServices(KinectSensorManager kinectSensorManager, KinectSensor sensor)
+        {
+            // Application should enable all streams first.
+
+            // configure the color stream
+            kinectSensorManager.ColorFormat = ColorImageFormat.RgbResolution640x480Fps30;
+            kinectSensorManager.ColorStreamEnabled = true;
+
+            // configure the depth stream
+            kinectSensorManager.DepthStreamEnabled = true;
+
+            kinectSensorManager.TransformSmoothParameters =
+                new TransformSmoothParameters
+                {
+                    Smoothing = 0.5f,
+                    Correction = 0.5f,
+                    Prediction = 0.5f,
+                    JitterRadius = 0.05f,
+                    MaxDeviationRadius = 0.04f
+                };
+
+            // configure the skeleton stream
+            sensor.SkeletonFrameReady += OnSkeletonFrameReady;
+            kinectSensorManager.SkeletonStreamEnabled = true;
+
+            // initialize the gesture recognizer
+            gestureController = new GestureController();
+            gestureController.GestureRecognized += OnGestureRecognized;
+
+            kinectSensorManager.KinectSensorEnabled = true;
+
+            if (!kinectSensorManager.KinectSensorAppConflict)
+            {
+                // addition configuration, as needed
+            }
+        }
+
+        /// <summary>
+        /// Kinect enabled apps should uninitialize all Kinect services that were initialized in InitializeKinectServices() here.
+        /// </summary>
+        /// <param name="sensor"></param>
+        private void UninitializeKinectServices(KinectSensor sensor)
+        {
+
+        }
+
+        #endregion Kinect Discovery & Setup
+
+        #region Properties
+
+        public static readonly DependencyProperty KinectSensorManagerProperty =
+            DependencyProperty.Register(
+                "KinectSensorManager",
+                typeof(KinectSensorManager),
+                typeof(MainWindow),
+                new PropertyMetadata(null));
+
+        public KinectSensorManager KinectSensorManager
+        {
+            get { return (KinectSensorManager)GetValue(KinectSensorManagerProperty); }
+            set { SetValue(KinectSensorManagerProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the last recognized gesture.
+        /// </summary>
+        private string _gesture;
+        public String Gesture
+        {
+            get { return _gesture; }
+
+            private set
+            {
+                if (_gesture == value)
+                    return;
+
+                _gesture = value;
+
+                Debug.WriteLine("Gesture = " + _gesture);
+
+                if (this.PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Gesture"));
+            }
+        }
+
+        #endregion Properties
+
+        #region Events
+
+        /// <summary>
+        /// Event implementing INotifyPropertyChanged interface.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+
+        #region Event Handlers
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">Gesture event arguments.</param>
+        private void OnGestureRecognized(object sender, GestureEventArgs e)
+        {
+            Debug.WriteLine(e.GestureType);
+
+            switch (e.GestureType)
+            {
+                case GestureType.Menu:
+                    Gesture = "Menu";
+                    break;
+                case GestureType.WaveRight:
+                    Gesture = "Wave Right";
+                    break;
+                case GestureType.WaveLeft:
+                    Gesture = "Wave Left";
+                    break;
+                case GestureType.JoinedHands:
+                    Gesture = "Joined Hands";
+                    break;
+                case GestureType.SwipeLeft:
+                    Gesture = "Swipe Left";
+                    break;
+                case GestureType.SwipeRight:
+                    Gesture = "Swipe Right";
+                    break;
+                case GestureType.ZoomIn:
+                    Gesture = "Zoom In";
+                    break;
+                case GestureType.ZoomOut:
+                    Gesture = "Zoom Out";
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame frame = e.OpenSkeletonFrame())
+            {
+                if (frame == null)
+                    return;
+
+                // resize the skeletons array if needed
+                if (skeletons.Length != frame.SkeletonArrayLength)
+                    skeletons = new Skeleton[frame.SkeletonArrayLength];
+
+                // get the skeleton data
+                frame.CopySkeletonDataTo(skeletons);
+
+                foreach (var skeleton in skeletons)
+                {
+                    // skip the skeleton if it is not being tracked
+                    if (skeleton.TrackingState != SkeletonTrackingState.Tracked)
+                        continue;
+
+                    // update the gesture controller
+                    gestureController.UpdateAllGestures(skeleton);
+                }
+            }
+        }
+
+        #endregion Event Handlers
+
+    }
+}
